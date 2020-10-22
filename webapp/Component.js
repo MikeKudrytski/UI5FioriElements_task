e21c021aacbb7d2444f40a149ef7b6d312234cef
Mike Kudritski <m.kudrytski@smartpro.by>
2020-10-22 08:29:25 +0000

    Init commit

---
+++ webapp/Component.js
@@ -0,0 +1,7 @@
+sap.ui.define(["sap/suite/ui/generic/template/lib/AppComponent"], function (AppComponent) {
+	return AppComponent.extend("UI5FioriElements_task.Component", {
+		metadata: {
+			"manifest": "json"
+		}
+	});
+});
