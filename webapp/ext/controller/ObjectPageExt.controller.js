3eff73849a7dc987f82292316c96b777f0abce1b
Mike Kudritski <m.kudrytski@smartpro.by>
2020-10-23 08:40:39 +0000

    12) Actions: activate, delete. Сompletion of paragraph 4

--- webapp/ext/controller/ObjectPageExt.controller.js
+++ webapp/ext/controller/ObjectPageExt.controller.js
@@ -1,10 +1,23 @@
 sap.ui.controller("UI5FioriElements_task.ext.controller.ObjectPageExt", {
 	onInit: function () {
-		this.extensionAPI.attachPageDataLoaded(function(oEvent){
-			var CategoryID = oEvent.context.getProperty("CategoryID")
+		this.extensionAPI.attachPageDataLoaded(function (oEvent) {
+			var CategoryID = oEvent.context.getProperty("CategoryID");
 			sap.m.MessageToast.show(CategoryID);
 		});
 	},
 
-	onClickActionCategoriesHeader1: function (oEvent) {}
+	onClickActionCategoriesHeader2: function (oEvent) {
+		sap.m.MessageToast.show("Hello World!!");
+	},
+	onClickActionCategoriesSections1: function (oEvent) {
+		this.getOwnerComponent().getModel().callFunction("/toActiveStatus", {
+			method: "GET",
+			success: function() {
+				this.getView();
+			}.bind(this),
+			error: function() {
+				sap.m.MessageToast.show("Error to Active Status!");
+			}
+		});
+	}
 });
